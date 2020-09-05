a1b6435f3e24532d2521331daa79a37b807b7c18
Donny Xia <xiadong.main@gmail.com>
2020-09-04 22:02:20 -0700

    init commit

---
+++ src/handlers.ts
@@ -0,0 +1,155 @@
+import TelegramBot from "node-telegram-bot-api";
+import CallOfDutyApi from "call-of-duty-api";
+
+const API = CallOfDutyApi({
+  ratelimit: { maxRequests: 2, perMilliseconds: 1000, maxRPS: 2 },
+});
+
+type HandlerRespType = "text" | "file";
+
+type HandlerResp = {
+  resp: string;
+  type: HandlerRespType;
+  fileName?: string;
+  contentType?: string;
+};
+
+type Handler = (msg: TelegramBot.Message) => Promise<HandlerResp>;
+
+const router = new Map<string, Handler>([
+  ["user", UserInfoHandler],
+  ["userRaw", UserInfoRawHandler],
+]);
+
+// command text format: /cmd arg1 arg2 ...
+export function getHandler(message: TelegramBot.Message): Handler {
+  const text = message.text;
+  if (!text) {
+    return unknownCommand;
+  }
+  let command = getCommand(text);
+  if (!command.startsWith("/")) {
+    return emptyHandler;
+  }
+  command = command.substring(1);
+  if (!router.has(command)) {
+    return unknownCommand;
+  }
+  return router.get(command)!;
+}
+
+export async function login(email: string, password: string) {
+  await API.login(email, password);
+}
+
+function getCommand(text: string | undefined): string {
+  if (!text) return "";
+  return text.split(" ")[0];
+}
+
+function getArgs(text: string | undefined): string[] {
+  if (!text) return [];
+  return text.split(" ").slice(1);
+}
+
+function getPlatform(p: string) {
+  switch (p) {
+    case "battle":
+      return API.platforms.battle;
+    case "psn":
+      return API.platforms.psn;
+    case "xbl":
+      return API.platforms.xbl;
+    default:
+      return API.platforms.all;
+  }
+}
+
+function constructUserInfo(data: any) {
+  return `\`\`\`
+---
+ALL
+---
+User: ${data.username}
+Game played: ${data.lifetime.mode.br.properties.gamesPlayed}
+Wins: ${data.lifetime.mode.br.properties.wins}
+Kills: ${data.lifetime.mode.br.properties.kills}
+Deaths: ${data.lifetime.mode.br.properties.deaths}
+Downs: ${data.lifetime.mode.br.properties.downs}
+K/D: ${data.lifetime.mode.br.properties.kdRatio}
+Top 5: ${data.lifetime.mode.br.properties.topFive}
+Top 10: ${data.lifetime.mode.br.properties.topTen}
+Top 25: ${data.lifetime.mode.br.properties.topTwentyFive}
+Accuracy (MP and WZ): ${data.lifetime.all.properties.accuracy}
+---
+Last week
+---
+Kills: ${data.weekly.mode.br_all.properties.kills}
+Deaths: ${data.weekly.mode.br_all.properties.deaths}
+K/D: ${data.weekly.mode.br_all.properties.kdRatio}
+Glug deaths: ${data.weekly.mode.br_all.properties.gulagDeaths}
+Glug kills: ${data.weekly.mode.br_all.properties.gulagKills}
+Team wiped: ${data.weekly.mode.br_all.properties.objectiveTeamWiped}
+Headshots: ${data.weekly.mode.br_all.properties.headshots}
+Headshot percentage: ${data.weekly.mode.br_all.properties.headshotPercentage}
+Kills per game: ${data.weekly.mode.br_all.properties.killsPerGame}
+Damage down: ${data.weekly.mode.br_all.properties.damageDone}
+Damage taken: ${data.weekly.mode.br_all.properties.damageTaken}
+\`\`\`
+`;
+}
+
+async function UserInfoHandler(
+  message: TelegramBot.Message
+): Promise<HandlerResp> {
+  const args = getArgs(message.text);
+  if (args.length !== 2) {
+    return {
+      resp: `Error: invalid arguments: ${args}`,
+      type: "text",
+    };
+  }
+  const [platform, user] = args;
+  const data = await API.MWwzstats(user, getPlatform(platform));
+  return {
+    resp: constructUserInfo(data),
+    type: "text",
+  };
+}
+
+async function UserInfoRawHandler(
+  message: TelegramBot.Message
+): Promise<HandlerResp> {
+  const args = getArgs(message.text);
+  if (args.length !== 2) {
+    return {
+      resp: `Error: invalid arguments: ${args}`,
+      type: "text",
+    };
+  }
+  const [platform, user] = args;
+  const data = await API.MWwzstats(user, getPlatform(platform));
+  return {
+    resp: JSON.stringify(data, null, 2),
+    type: "file",
+    fileName: "userData.json",
+    contentType: "text/json",
+  };
+}
+
+async function unknownCommand(
+  message: TelegramBot.Message
+): Promise<HandlerResp> {
+  const command = getCommand(message.text!);
+  return {
+    resp: `unknown command: ${command}`,
+    type: "text",
+  };
+}
+
+async function emptyHandler(): Promise<HandlerResp> {
+  return {
+    resp: "",
+    type: "text",
+  };
+}
---
+++ src/index.ts
@@ -0,0 +1,122 @@
+import TelegramBot from "node-telegram-bot-api";
+import { exit, hasUncaughtExceptionCaptureCallback } from "process";
+import { ArgumentParser } from "argparse";
+import readline from "readline";
+import { readFileSync, fstat } from "fs";
+import { getHandler, login } from "./handlers";
+
+const parser = new ArgumentParser({
+  description: "Telegram COD 16 Warzone Data Bot",
+});
+
+parser.add_argument("--local", {
+  default: "false",
+  choices: ["false", "true"],
+});
+parser.add_argument("--config", {
+  required: true,
+});
+
+function auth(id: string | undefined, adminId: string): boolean {
+  return id === adminId;
+}
+
+type Config = {
+  BOT_TOKEN: string;
+  ACTIVISION_PASSWORD: string;
+  ACTIVISION_EMAIL: string;
+  ADMIN_ID: string;
+};
+
+function getConfig(path: string): Config {
+  return JSON.parse(readFileSync(path).toString()) as Config;
+}
+
+async function localMain() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  while (true) {
+    const answer = await new Promise((resolve) =>
+      rl.question("Command: ", (answer) => resolve(answer))
+    );
+    if (answer === "") break;
+    const message = {
+      text: answer,
+      message_id: 0,
+      date: 0,
+      chat: {
+        id: 0,
+        type: "private",
+      },
+    } as TelegramBot.Message;
+    const h = getHandler(message);
+    const resp = await h(message);
+    console.log(resp);
+  }
+}
+
+async function main() {
+  const args = parser.parse_args();
+  const config = getConfig(args.config);
+  const TOKEN = config.BOT_TOKEN;
+
+  if (TOKEN === undefined) {
+    console.error("No bot token provided");
+    exit();
+  }
+
+  const bot = new TelegramBot(TOKEN, { polling: true });
+
+  await login(config.ACTIVISION_EMAIL, config.ACTIVISION_PASSWORD);
+
+  if (args.local === "true") {
+    await localMain();
+    return;
+  }
+
+  // query a user information
+  bot.on(
+    "message",
+    async (message: TelegramBot.Message, metadata: TelegramBot.Metadata) => {
+      if (!auth(message.from?.id.toString(), config.ADMIN_ID)) return;
+      const chatId = message.chat.id;
+      if (metadata.type !== "text") {
+        bot.sendMessage(chatId, `Unsupport type: ${metadata.type}`);
+        return;
+      }
+      let resp;
+      try {
+        const h = getHandler(message);
+        resp = await h(message);
+        if (resp.resp.length === 0) {
+          return;
+        }
+      } catch (err) {
+        bot.sendMessage(chatId, `Error: ${err}`);
+        return;
+      }
+      if (resp.type === "text")
+        bot.sendMessage(chatId, resp.resp, {
+          parse_mode: "MarkdownV2",
+        });
+      else
+        bot.sendDocument(
+          chatId,
+          Buffer.from(resp.resp),
+          {},
+          {
+            filename: resp.fileName,
+            contentType: resp.contentType,
+          }
+        );
+    }
+  );
+}
+
+main().catch((err) => {
+  console.error(err);
+  exit(1);
+});
